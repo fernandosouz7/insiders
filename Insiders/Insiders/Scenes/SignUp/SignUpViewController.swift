c9aa2c712ea739def7427d6513e20504f94e548b
fernandoss7 <fernando.souza@invillia.com>
2022-01-17 11:43:48 -0300

    feature/tela-de-cadastro

--- Insiders/Insiders/Scenes/SignUp/SignUpViewController.swift
+++ Insiders/Insiders/Scenes/SignUp/SignUpViewController.swift
@@ -3,24 +3,28 @@ import UIKit
 final class SignUpViewController: BaseViewController {
 
     private var viewModel: SignUpViewModel?
-
+    
     init(viewModel: SignUpViewModel) {
-            self.viewModel = viewModel
-            super.init(nibName: nil, bundle: nil)
-        }
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
 
-        required init?(coder: NSCoder) {
-            super.init(coder: coder)
-        }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 
-        override func loadView() {
-            view = SignUpView()
-        }
+    override func loadView() {
+        view = SignUpView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         super.setupLeftBarButton(selector: #selector(didTapBackButton))
         setupNavigation(isHidden: false)
+        setupTitle()
+    }
+
+    private func setupTitle() {
         self.title = "Criar Conta"
     }
 
